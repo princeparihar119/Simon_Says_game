8fafb53b63493a7acaef838d4279ebb3d0fed173
Prince Singh Parihar <133567027+princeparihar119@users.noreply.github.com>
2024-07-10 11:24:33 +0530

    Update app.js
    style change

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ function checkAns(idx) {
         h2.innerHTML = `Game Over! Your Score Was <b>${level}</b> <br> Press any key to start.`;
         document.querySelector("body").style.backgroundColor = "red";
         setTimeout(function() {
-            document.querySelector("body").style.backgroundColor = "white";
+            document.querySelector("body").style.backgroundColor = "#965ef7";
         },150);
         reset();
     }
